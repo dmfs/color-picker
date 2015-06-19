fdd870e5f7ba821a74b128679765589362e2a7db
Marten Gajda <marten@dmfs.org>
2015-06-19 12:47:40 +0200

    Add material colors to ColorPickerActivity

--- demo/src/org/dmfs/android/colorpicker/demo/DemoActivity.java
+++ demo/src/org/dmfs/android/colorpicker/demo/DemoActivity.java
@@ -41,10 +41,12 @@ public class DemoActivity extends FragmentActivity implements ColorDialogResultL
 {
 	private final static int[] COLORS = new int[] { 0xff000000, 0xff0000ff, 0xff00ff00, 0xffff0000, 0xffffff00, 0xff00ffff, 0xffff00ff, 0xff404040, 0xff808080,
 		0xff8080ff, 0xff80ff80, 0xffff8080, 0xffffff80, 0xff80ffff, 0xffff80ff, 0xffffffff, };
-	private final static int[] MATERIAL_COLORS = new int[] { 0xffe51c23, 0xffff5722, 0xffff9800, 0xffffc107, 0xffffeb3b, 0xffcddc39, 0xff8bc34a, 0xff259b24,
-		0xff009688, 0xff00bcd4, 0xff03a9f4, 0xff5677fc, 0xff3f51b5, 0xff673ab7, 0xff9c27b0, 0xffe91e63,
 
-		0xff795548, 0xff9e9e9e, 0xff607d8b };
+	private final static int[] MATERIAL_COLORS_PRIMARY = { 0xffe91e63, 0xfff44336, 0xffff5722, 0xffff9800, 0xffffc107, 0xffffeb3b, 0xffcddc39, 0xff8bc34a,
+		0xff4caf50, 0xff009688, 0xff00bcd4, 0xff03a9f4, 0xff2196f3, 0xff3f51b5, 0xff673ab7, 0xff9c27b0 };
+
+	private static final int MATERIAL_COLORS_SECONDARY[] = { 0xffad1457, 0xffc62828, 0xffd84315, 0xffef6c00, 0xffff8f00, 0xfff9a825, 0xff9e9d24, 0xff558b2f,
+		0xff2e7d32, 0xff00695c, 0xff00838f, 0xff0277bd, 0xff1565c0, 0xff283593, 0xff4527a0, 0xff6a1b9a };
 
 	private TextView mTextView;
 	private View mColorBox;
@@ -69,14 +71,15 @@ public class DemoActivity extends FragmentActivity implements ColorDialogResultL
 
 		ArrayList<AbstractPalette> palettes = new ArrayList<AbstractPalette>();
 
+		palettes.add(new ArrayPalette("material_primary", "Material Colors", MATERIAL_COLORS_PRIMARY, 4));
+		palettes.add(new ArrayPalette("material_secondary", "Dark Material Colors", MATERIAL_COLORS_SECONDARY, 4));
+
 		// add a palette from the resources
 		palettes.add(ArrayPalette.fromResources(this, "base", R.string.base_palette_name, R.array.base_palette_colors, R.array.base_palette_color_names));
 
 		palettes.add(new ArrayPalette("base2", "Base 2", COLORS));
 
 		// Android Material color schema
-		palettes.add(new ArrayPalette("Material", "Material", MATERIAL_COLORS));
-
 		// Add a palette with rainbow colors
 		palettes.add(new FactoryPalette("rainbow", "Rainbow", ColorFactory.RAINBOW, 16));
 
